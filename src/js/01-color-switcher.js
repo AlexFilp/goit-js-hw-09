f64aaeb6089a8d2f70ac2c4ae1847918d69aca6e
Oleksandr FIlippov <filya170895@gmail.com>
2022-12-25 12:45:29 +0100

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.body,
+};
 
+let isActive = false;
+let intervalId = null;
+
+refs.startBtn.addEventListener('click', makeRandomColor);
+
+function makeRandomColor() {
+  if (isActive) {
+    return;
+  }
+  isActive = true;
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+refs.stopBtn.addEventListener('click', stopRandomColor);
+
+function stopRandomColor() {
+  clearInterval(intervalId);
+  isActive = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
