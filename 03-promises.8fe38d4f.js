0e91df6b252089f94ff3f9b6fe4263a7647b05e1
AlexFilp <110115726+AlexFilp@users.noreply.github.com>
2022-12-27 19:14:55 +0000

    Deploying to gh-pages from @ AlexFilp/goit-js-hw-09@9c3914234ee833ffcc0e3794b8f08d4e8972afbe 🚀

--- 03-promises.8fe38d4f.js
+++
@@ -1,2 +0,0 @@
-const o=document.querySelector(".form");console.dir(o),console.log("HELLO");
-//# sourceMappingURL=03-promises.8fe38d4f.js.map
--- 03-promises.e41ee10d.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=document.querySelector(".form");console.dir(o),console.log("HELLO")}();
-//# sourceMappingURL=03-promises.e41ee10d.js.map
