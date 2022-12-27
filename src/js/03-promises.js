05866943e21eac0182d52938273a2bfaa14d9769
Oleksandr FIlippov <filya170895@gmail.com>
2022-12-27 15:01:36 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,7 +2,6 @@ const form = document.querySelector('.form');
 
 console.dir(form);
 console.log('HELLO');
-
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
